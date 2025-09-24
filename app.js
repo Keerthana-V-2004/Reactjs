7a8d26b91e9f4c1f866b3843dd010309a2454f2d
Keerthana V <keertanv1975@gmail.com>
2025-09-24 15:03:20 +0530

    import react module instead of cdn

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
 
-
+const container = React.createElement("h1");
+  const root = ReactDOM.createRoot("container");
+  root.render(container);
